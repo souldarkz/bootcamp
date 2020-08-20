95576ef252d40a03b5d51ea354f6fb9ced3c3828
Trebor87j <jhonrobert1987@gmail.com>
2020-08-19 23:42:25 -0500

    Generación de código base en Productos.js

---
+++ pantallas/Productos.js
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export class Productos extends Component{
+  render(){
+    return <View>
+      <Text>PRODUCTOS</Text>
+    </View>
+  }
+}
